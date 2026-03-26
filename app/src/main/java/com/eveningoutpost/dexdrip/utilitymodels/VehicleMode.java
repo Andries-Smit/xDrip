c5c11222fedde0e4f49a36315aa234dd4515fd5b
Andries Smit <asmit@clevr.com>
2026-03-26 22:10:27 +0100

    feat: start floating widget in car mode

--- app/src/main/java/com/eveningoutpost/dexdrip/utilitymodels/VehicleMode.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utilitymodels/VehicleMode.java
@@ -9,14 +9,21 @@ package com.eveningoutpost.dexdrip.utilitymodels;
 // TODO move elements relating only to vehicle mode from ActivityRecognizedService to here
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
+import com.eveningoutpost.dexdrip.models.UserError;
 import com.eveningoutpost.dexdrip.services.ActivityRecognizedService;
+import com.eveningoutpost.dexdrip.services.FloatingViewService;
+import com.eveningoutpost.dexdrip.xdrip;
 
 import static com.eveningoutpost.dexdrip.utilitymodels.Intents.ACTION_VEHICLE_MODE;
 import static com.eveningoutpost.dexdrip.utilitymodels.Intents.EXTRA_VEHICLE_MODE_ENABLED;
 
 public class VehicleMode {
+
+    private static final String TAG = VehicleMode.class.getSimpleName();
 
     public static boolean isEnabled() {
         return Pref.getBooleanDefaultFalse("vehicle_mode_enabled");
@@ -45,6 +52,30 @@ public class VehicleMode {
 
     public static void setVehicleModeActive(final boolean active) {
         ActivityRecognizedService.set_vehicle_mode(active);
+        if (Pref.getBooleanDefaultFalse("vehicle_mode_floating_widget")) {
+            if (active) {
+                startFloatingWidget();
+            } else {
+                stopFloatingWidget();
+            }
+        }
+    }
+
+    private static void startFloatingWidget() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !Settings.canDrawOverlays(xdrip.getAppContext())) {
+            UserError.Log.e(TAG, "Cannot show floating widget: overlay permission not granted");
+            return;
+        }
+        Pref.setBoolean("show_floating_widget", true);
+        xdrip.getAppContext().startService(
+                new Intent(xdrip.getAppContext(), FloatingViewService.class));
+    }
+
+    private static void stopFloatingWidget() {
+        Pref.setBoolean("show_floating_widget", false);
+        xdrip.getAppContext().stopService(
+                new Intent(xdrip.getAppContext(), FloatingViewService.class));
     }
 
     public static void sendBroadcast() {
